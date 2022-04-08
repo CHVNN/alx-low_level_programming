312bb33b004a4f6dadf32e1d2ba3d9a2edd3f0c2
CHVNN <iwuohaustin95@gmail.com>
2022-04-08 15:42:37 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints in positive, negative
+ * or zero
+ * Return: returns zero if program exits well
+ */
+
+int main(void)
+{
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
