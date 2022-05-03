68e0f45fa250de4ab65d7557aa1cdae132570a95
CHVNN <iwuohaustin95@gmail.com>
2022-05-02 17:23:55 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - prints all all the
+* arguments it receives.
+* @argc: offset count
+* @argv: offset value
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
