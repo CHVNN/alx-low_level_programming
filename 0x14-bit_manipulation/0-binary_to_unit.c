8449edff4c461fd55e6a88940d09e54feadef902
CHVNN <iwuohaustin95@gmail.com>
2022-05-31 12:55:48 -0700

    0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -10,29 +10,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+int i;
+unsigned int n;
 
-	if (!b)
-		return (0);
+i = 0;
+n = 0;
 
-	ui = 0;
+if (!b)
+return (0);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-
-	return (ui);
+while (b[i] != '\0')
+{
+if (b[i] == '1')
+{
+n = n * 2 + 1;
+}
+else if (b[i] == '0')
+{
+n = n * 2;
+}
+else
+{
+return (0);
+}
+i++;
+}
+return (n);
 }
